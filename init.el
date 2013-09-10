6e61de9af8f5dff3bd2540ed9452b92de4cd4dbf
gladiator <lishuaihenu@gmail.com>
2013-09-10 17:16:54 +0800

    linum-format

--- init.el
+++ init.el
@@ -161,17 +161,16 @@
 (require 'linum)
 (global-linum-mode t)
 
+
+
+
 (when (not window-system)
   (xterm-mouse-mode t)
-  (add-hook 'linum-before-numbering-hook
-	    (lambda ()
-	      (setq-default linum-format
-			    (concat "%"
-				    (number-to-string
-				     (length (number-to-string
-					      (count-lines
-					       (point-min) (point-max)))))
-				    "d ")))))
+  (defadvice linum-update-window (around linum-format-dynamic activate)
+    (let* ((w (length (number-to-string
+		       (count-lines (point-min) (point-max)))))
+	   (linum-format (concat "%" (number-to-string w) "d ")))
+      ad-do-it)))
 
 (require 'whitespace)
 (global-set-key [f6] 'whitespace-mode)
--- themes/calm-forest-theme.el
+++ themes/calm-forest-theme.el
@@ -132,7 +132,7 @@
  '(jde-java-font-lock-package-face ((t (:foreground "steelblue1"))))
  '(jde-java-font-lock-pre-face ((t (nil))))
  '(jde-java-font-lock-underline-face ((t (:underline t))))
- '(linum ((t (:background "color-232"))))
+ '(linum ((t (:background "color-235"))))
  '(menu ((t (nil))))
  '(mode-line ((t (:background "grey75" :foreground "black"
 			      :box (:line-width -1 :style released-button)))))
